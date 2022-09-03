919d1a8fe29d1c8aaaf1cbd68a32a8c04a0361c6
davi bandeira <fcodavibandeira@edu.unifor.br>
2022-09-03 12:15:27 -0300

    initial character attack/spawn effect logic

--- scripts/character/character.gd
+++ scripts/character/character.gd
@@ -4,6 +4,9 @@ class_name Character
 onready var stats: Stats = get_node("Stats")
 onready var animation: AnimationPlayer = get_node("Animation")
 
+var target = null
+var attack_damage: int
+
 func update_health(damage: int) -> void:
 	stats.health = clamp(stats.health - damage, 0, stats.max_health)
 	if stats.health == 0:
@@ -11,6 +14,34 @@ func update_health(damage: int) -> void:
 		return
 		
 	animation.play("hit")
+	
+	
+func attack(attack_type: String, attack_target) -> void:
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
+	target = attack_target
+	
+	match attack_type:
+		"normal":
+			attack_damage = rng.randi_range(
+				stats.normal_attack_gap.min(), 
+				stats.normal_attack_gap.max()
+			)
+			
+		"special":
+			attack_damage = rng.randi_range(
+				stats.special_attack_gap.min(), 
+				stats.special_attack_gap.max()
+			)
+			
+	
+	animation.play(attack_type)
+	
+	
+func spawn_projectile() -> void:
+	#target.update_health(attack_damage)
+	pass
 	
 	
 func on_animation_finished(_anim_name: String) -> void:
--- scripts/character/stats.gd
+++ scripts/character/stats.gd
@@ -1,5 +1,8 @@
 extends Node
 class_name Stats
 
+export(Array, int) var normal_attack_gap
+export(Array, int) var special_attack_gap
+
 export(int) var health
 export(int) var max_health
