3eb4d47fc8d2e3a51ac46ca195c7eed63421af2b
davi bandeira <fcodavibandeira@edu.unifor.br>
2022-09-09 21:26:56 -0300

    adding bgm/music and background music script/scene

---
+++ scripts/management/background_music.gd
@@ -0,0 +1,23 @@
+extends AudioStreamPlayer
+class_name BackgroundMusic
+
+var maximum_index: int = 16
+
+var theme: String = "theme_"
+var base_path: String = "res://assets/music/"
+
+func _ready() -> void:
+	randomize()
+	stream_random_music()
+	
+	
+func stream_random_music() -> void:
+	var random_index: int = randi() % maximum_index + 1
+	var sound_to_stream: String = base_path + theme + str(random_index) + ".ogg"
+	
+	set_stream(load(sound_to_stream))
+	play()
+	
+	
+func on_background_music_finished() -> void:
+	stream_random_music()
